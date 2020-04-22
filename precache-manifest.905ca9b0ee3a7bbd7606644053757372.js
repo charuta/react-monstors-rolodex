32ce21384db2f2b3b48f3798b7cf3824ac97a2a6
Charuta Rajopadhye <charuta.rajopadhye@anthem.com>
2020-04-22 16:31:38 -0400

    Updates

---
+++ precache-manifest.905ca9b0ee3a7bbd7606644053757372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047d0468b0912c2108e7be0ad4c50722",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2bbd50333d6f1b2f3ef3",
+    "url": "/react-monsters-rolodex/static/css/main.fe0ad9ff.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbd50333d6f1b2f3ef3",
+    "url": "/react-monsters-rolodex/static/js/main.16c6f88b.chunk.js"
+  },
+  {
+    "revision": "84d462a1ad20fdb403f4",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.6eda5f3f.js"
+  }
+]);
